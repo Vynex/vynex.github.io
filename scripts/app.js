a55aa3f793bafe5efa69c4ea7ddc13da5720e799
Kushagra <kushagra100910@gmail.com>
2021-01-21 16:24:46 +0530

    v0.30
    long-overdue update

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+
+    sectionsColor: ['#8a2be2', '#448aff', '#fdb827', '#ec407a', "#4caf50"],
+    anchors: ['Home', 'About', 'Skills', 'Portfolio', 'Links'],
+});
