6f37665402df43dd83414b7a1135a39a3630e61e
Kushagra <kushagra100910@gmail.com>
2021-01-22 20:14:04 +0530

    added responsive elements

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,23 @@ new fullpage('#fullpage', {
     sectionsColor: ['#8a2be2', '#448aff', '#fdb827', '#ec407a', "#4caf50"],
     anchors: ['Home', 'About', 'Skills', 'Portfolio', 'Links'],
 });
+
+
+var burger = document.querySelector('.burger');
+var nav = document.querySelector('.collapsable');
+var navLinks = document.querySelectorAll('.collapsable li');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('navActive');
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        }
+        else {
+            link.style.animation = `navLinksAnim 0.5s ease forwards ${index / 7 + 0.2}s`
+        }
+    });
+
+    burger.classList.toggle('toggle');
+});
