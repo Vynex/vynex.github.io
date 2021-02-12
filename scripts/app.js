dea0a37886de2b3b87cf2588ec93614ada38a417
Vynex <kushagra100910@gmail.com>
2021-02-12 20:26:05 +0530

    Added Skills

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ new fullpage('#fullpage', {
     //options here
     autoScrolling:true,
 
-    sectionsColor: ['#8a2be2', '#448aff', '#fdb827', '#ec407a', "#4caf50"],
+    sectionsColor: ['#8a2be2', '#448aff', '#eb5e0b', '#ec407a', "#4caf50"],
     anchors: ['Home', 'About', 'Skills', 'Portfolio', 'Links'],
 });
 
