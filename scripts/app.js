e5476c6da5aec57d793e360a650645138898bd6b
Vynex <kushagra100910@gmail.com>
2021-03-24 14:24:51 +0530

    minor colour improvements

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,18 @@
 new fullpage('#fullpage', {
-    //options here
-    autoScrolling:true,
+	autoScrolling: true,
 
-    sectionsColor: ['#8a2be2', '#448aff', '#ff5722', '#ec407a', "#4caf50"],
-    anchors: ['Home', 'About', 'Skills', 'Portfolio', 'Links'],
+	sectionsColor: ['#845ec2', '#51c2d5', '#f48b29', '#ff5e78', '#16c79a'],
+	anchors: ['Home', 'About', 'Skills', 'Portfolio', 'Links'],
 });
 
 
 const nav = document.querySelector('.collapsable');
 const navLinks = document.querySelectorAll('.collapsable li');
 const burger = document.querySelector('.burger');
+const image = document.querySelector('.avatar');
 
 burger.addEventListener('click', () => {
-    nav.classList.toggle('navActive');
+    nav.classList.toggle('nav-active');
 
     navLinks.forEach((link, index) => {
         if (link.style.animation)
@@ -20,6 +20,11 @@ burger.addEventListener('click', () => {
         else
             link.style.animation = `navLinksAnim 0.5s ease forwards ${index / 7 + 0.2}s`
     });
+
+    if (image.style.animation)
+        image.style.animation = '';
+    else
+        image.style.animation = `navLinksAnim 0.5s ease forwards ${4 / 7 + 0.3}s`
 
     burger.classList.toggle('toggle');
 });
--- scripts/ml11.js
+++ scripts/ml11.js
@@ -1,31 +1,31 @@
 // Wrap every letter in a span
 //taken from ml11
-var textWrapper = document.querySelector('.movingLetters .letters');
+var textWrapper = document.querySelector('.moving-letters .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.movingLetters .line',
+    targets: '.moving-letters .line',
     scaleY: [0,1],
     opacity: [0.5,1],
     easing: "easeOutExpo",
     duration: 700
   })
   .add({
-    targets: '.movingLetters .line',
-    translateX: [0, document.querySelector('.movingLetters .letters').getBoundingClientRect().width + 10],
+    targets: '.moving-letters .line',
+    translateX: [0, document.querySelector('.moving-letters .letters').getBoundingClientRect().width + 10],
     easing: "easeOutExpo",
     duration: 700,
     delay: 100
   }).add({
-    targets: '.movingLetters .letter',
+    targets: '.moving-letters .letter',
     opacity: [0,1],
     easing: "easeOutExpo",
     duration: 600,
     offset: '-=775',
     delay: (el, i) => 34 * (i+1)
   }).add({
-    targets: '.movingLetters',
+    targets: '.moving-letters',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
