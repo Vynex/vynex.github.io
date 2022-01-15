73f5bc956a061e614cd407fb41efdb87bea525c0
Vynex <kushagra100910@gmail.com>
2022-01-15 12:48:54 +0530

    updated website

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,9 @@
 new fullpage('#fullpage', {
 	autoScrolling: true,
 
-	sectionsColor: ['#845ec2', '#51c2d5', '#f48b29', '#ff5e78', '#16c79a'],
-	anchors: ['Home', 'About', 'Skills', 'Portfolio', 'Links'],
+	sectionsColor: ['#845ec2', '#51c2d5', '#ff5e78', '#16c79a'],
+	anchors: ['Home', 'About', 'Skills', 'Links'],
 });
-
 
 const nav = document.querySelector('.collapsable');
 const navLinks = document.querySelectorAll('.collapsable li');
@@ -12,19 +11,19 @@ const burger = document.querySelector('.burger');
 const image = document.querySelector('.avatar');
 
 burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+	nav.classList.toggle('nav-active');
 
-    navLinks.forEach((link, index) => {
-        if (link.style.animation)
-            link.style.animation = '';
-        else
-            link.style.animation = `navLinksAnim 0.5s ease forwards ${index / 7 + 0.2}s`
-    });
+	navLinks.forEach((link, index) => {
+		if (link.style.animation) link.style.animation = '';
+		else
+			link.style.animation = `navLinksAnim 0.5s ease forwards ${
+				index / 7 + 0.2
+			}s`;
+	});
 
-    if (image.style.animation)
-        image.style.animation = '';
-    else
-        image.style.animation = `navLinksAnim 0.5s ease forwards ${4 / 7 + 0.3}s`
+	if (image.style.animation) image.style.animation = '';
+	else
+		image.style.animation = `navLinksAnim 0.5s ease forwards ${4 / 7 + 0.3}s`;
 
-    burger.classList.toggle('toggle');
+	burger.classList.toggle('toggle');
 });
